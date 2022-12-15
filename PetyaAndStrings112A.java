8a15c5282f2f8760c9c1fe9c01384ada50ab625d
heejinq1 <69579123+heejinq1@users.noreply.github.com>
2022-12-15 23:23:20 +0200

    Update and rename 112APetyaAndStrings.java to PetyaAndStrings112A.java

--- PetyaAndStrings112A.java
+++ PetyaAndStrings112A.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class 112APetyaAndStrings{
+public class PetyaAndStrings112A{
     public static void main (String args[])
 {
     Scanner sc = new Scanner(System.in);
